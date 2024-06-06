102da0e7ce93f0e87594c64f637f1179d16894ce
Galya <glitkin@gmail.com>
2024-06-06 19:52:05 +0200

    Add .idea to gitignore

--- src/beam_app/clean_tweet.py
+++ src/beam_app/clean_tweet.py
@@ -3,8 +3,15 @@ import apache_beam as beam
 
 
 class CleanTweet(beam.DoFn):
-    def process(self, element):
-        tweet = re.sub(r"http\S+", "", element)  # Remove URLs
-        tweet = re.sub(r"RT\s+@\w+", "", tweet)  # Remove 'RT @user'
-        tweet = tweet.replace('#', '')  # Remove hashtags
+    def process(self, tweet):
+        print(f"\nInput: {tweet}")
+        cleaning_steps = [
+            (r"http\S+", ""),  # Remove URLs
+            (r"\bRT\s?:\s?", ""),  # Remove 'RT :' and 'RT:'
+            (r"#", "")  # Remove hashtags
+        ]
+
+        for pattern, replacement in cleaning_steps:
+            tweet = re.sub(pattern, replacement, tweet)
+        print(f"Output: {tweet}")
         return [tweet.strip()]
